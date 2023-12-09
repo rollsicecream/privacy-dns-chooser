cdfb8e4e0e103469bd3a5bbcb90005cd07c18dd8
rollsicecream <153316540+rollsicecream@users.noreply.github.com>
2023-12-09 13:33:14 +0100

    Add privacy_dns_chooser.sh - Initial release.
    The privacy DNS chooser script for Linux has been released.

---
+++ privacy_dns_chooser.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo "Welcome to the Privacy DNS chooser script for Linux."
+echo "This script will help you enable DoT with a privacy DNS provider on your Linux system."
+
+echo "Be AWARE that the script will not run properly if the systemd-resolved package is not installed on your Linux system."
+echo "Most distros nowadays do include it, but in some cases, your distribution may not come with it. Please check before running this script."
+echo "To check it, do: sudo <your package manager, like apt or dnf> systemd-resolved. If it's already installed, you're good, if not, install it."
+
+# Prompt user for DNS provider choice
+echo "Choose a DNS provider:"
+echo "1. Quad9"
+echo "2. Mullvad DNS"
+echo "3. NextDNS"
+
+read -p "Enter the number corresponding to your choice: " choice
+
+case $choice in
+    1)
+        dns_provider="9.9.9.9#dns.quad9.net"
+        ;;
+    2)
+        dns_provider="194.242.2.2#dns.mullvad.net"
+        ;;
+    3)
+        echo "You've selected NextDNS."
+        read -p "Enter your NextDNS configuration number (e.g., a12345): " nextdns_config
+        dns_provider="45.90.28.0#$nextdns_config.dns.nextdns.io"
+        ipv6_provider="2a07:a8c0::#$nextdns_config.dns.nextdns.io"
+        dns_provider2="45.90.30.0#$nextdns_config.dns.nextdns.io"
+        ipv6_provider2="2a07:a8c1::#$nextdns_config.dns.nextdns.io"
+        ;;
+    *)
+        echo "Invalid choice. Exiting."
+        echo "Your DNS provider has not been set due to errors. Do you have systemd-resolved installed on your system?"
+        exit 1
+        ;;
+esac
+
+resolved_conf="/etc/systemd/resolved.conf"
+
+# Uncomment and modify DNS and DNSOverTLS settings (disable DNSSEC)
+sudo sed -i -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNS=\).*/\1$dns_provider/" \
+            -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNSOverTLS=\).*/\1yes/" \
+            -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNSSEC=\).*/\1no/" \
+            -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNSSECStub=\).*/\1no/" $resolved_conf
+
+if [ "$choice" -eq 3 ]; then
+    # Add additional DNS lines for NextDNS
+    echo "Adding NextDNS configuration to resolved.conf..."
+    sudo tee -a $resolved_conf > /dev/null <<EOL
+DNS=$ipv6_provider
+DNS=$dns_provider2
+DNS=$ipv6_provider2
+EOL
+fi
+
+# Enable and start the systemd-resolved service
+if sudo systemctl enable systemd-resolved && sudo systemctl restart systemd-resolved; then
+    echo "You're all set! Your DNS has been set, and DNS-over-TLS has been enabled for security measures."
+else
+    echo "Your DNS provider has not been set due to errors. Do you have systemd-resolved installed on your system?"
+fi
+
