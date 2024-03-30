12d0e8325d28145536e11a719310d8df582a3de9
rollsicecream <rollsicecream@proton.me>
2024-03-30 12:38:48 +0100

    v1.3 release

--- privacy_dns_chooser.sh
+++ privacy_dns_chooser.sh
@@ -1,75 +1,83 @@
 #!/bin/bash
 
-echo "Welcome to the Privacy DNS chooser script for Linux version v1.2."
-echo "This script will help you enable DoT with a privacy DNS provider on your Linux system."
+# Variables declaration
+RED='\033[0;31m' # red color
+NC='\033[0m' # no color to terminate red start
+resolved_conf="/etc/systemd/resolved.conf" # path to systemd-resolved configuration file
 
-echo "Be AWARE that the script will not run properly if the systemd-resolved package is not installed on your Linux system."
-echo "Most distros nowadays do include it, but in some cases, your distribution may not come with it. Please check before running this script."
-echo "To check it, do: sudo <your package manager, like apt or dnf> systemd-resolved. If it's already installed, you're good, if not, install it."
+# Checking if user is root
+if [ $EUID != 0 ]; then
+	printf "Make sure you're running this script as ${RED}ROOT${NC}\n"
+	exit 1
+fi
+
+printf "Welcome to the Privacy DNS chooser script for Linux version v1.3.\n"
+printf "This script will help you enable ${RED}DoT${NC} with a privacy ${RED}DNS${NC} provider on your Linux system.\n"
+printf "Be ${RED}AWARE${NC} that the script will not run properly if the ${RED}systemd-resolved${NC} package is not installed on your Linux system.\n"
+printf "Most distros nowadays do include it, but in some cases, your distribution may not come with it. Please ${RED}check${NC} before running this script.\n"
+printf "To check it, do: ${RED}sudo${NC} <your package manager, like ${RED}apt${NC} or ${RED}dnf${NC}> systemd-resolved. If it's already installed, you're good, if not, install it.\n"
 
 # Prompt user for DNS provider choice
-echo "Choose a DNS provider:"
-echo "1. Quad9 [RECOMMENDED]"
-echo "2. Mullvad DNS"
-echo "3. Mullvad DNS (ad, tracker, and malware blocking) [RECOMMENDED]"
-echo "4. AdGuard DNS"
-echo "5. NextDNS [RECOMMENDED]"
+printf "Choose a DNS provider:\n"
+printf "1. ${RED}Quad9${NC} [RECOMMENDED]\n"
+printf "2. ${RED}Mullvad DNS${NC}\n"
+printf "3. ${RED}Mullvad DNS${NC} (ad, tracker, and malware blocking) [RECOMMENDED]\n"
+printf "4. ${RED}AdGuard DNS${NC}\n"
+printf "5. ${RED}NextDNS${NC} [RECOMMENDED]\n"
 
-read -p "Enter the number corresponding to your choice: " choice
+printf "Enter the number corresponding to your choice: "; read choice
 
 case $choice in
     1)
         dns_provider="9.9.9.9#dns.quad9.net"
-        provider_name="Quad9 [RECOMMENDED]"
+        provider_name="${RED}Quad9${NC} [RECOMMENDED]"
         ;;
     2)
         dns_provider="194.242.2.2#dns.mullvad.net"
-        provider_name="Mullvad DNS"
+        provider_name="${RED}Mullvad DNS${NC}"
         ;;
     3)
         dns_provider="194.242.2.4#base.dns.mullvad.net"
-        provider_name="Mullvad DNS (ad, tracker, and malware blocking) [RECOMMENDED]"
+        provider_name="${RED}Mullvad DNS (ad, tracker, and malware blocking)${NC} [RECOMMENDED]"
         ;;    
     4)  
         dns_provider="94.140.14.14#dns.adguard-dns.io"
-        provider_name="AdGuard DNS"
+        provider_name="${RED}AdGuard DNS${NC}"
         ;;
     5)
-        echo "You've selected NextDNS."
-        read -p "Enter your NextDNS configuration number (e.g., a12345): " nextdns_config
+        printf "You've selected ${RED}NextDNS${NC}.\n"
+        provider_name="${RED}NextDNS${NC} [RECOMMENDED]"
+        printf "Enter your $provider_name configuration number (e.g., a12345): "; read nextdns_config
         dns_provider="45.90.28.0#$nextdns_config.dns.nextdns.io"
         ipv6_provider="2a07:a8c0::#$nextdns_config.dns.nextdns.io"
         dns_provider2="45.90.30.0#$nextdns_config.dns.nextdns.io"
         ipv6_provider2="2a07:a8c1::#$nextdns_config.dns.nextdns.io"
-        provider_name="NextDNS [RECOMMENDED]"
         ;;
     *)
-        echo "Invalid choice. Exiting."
-        echo "Your DNS provider has not been set due to errors. Put the right number : 1, 2, 3, 4 or 5."
+        printf "Invalid choice. Exiting.\n"
+        printf "Your ${RED}DNS${NC} provider has not been set due to errors. Put the right number: 1, 2, 3, 4 or 5."
         exit 1
         ;;
 esac
 
 # Confirm the user's choice before making changes
-read -p "You've selected $provider_name. Are you sure you want to change your DNS provider? (yes/no): " confirmation
+printf "You've selected $provider_name.\nAre you sure you want to change your ${RED}DNS${NC} provider? (yes/no): "; read confirmation
 
-if [[ "$confirmation" != "yes" ]]; then
-    echo "DNS change aborted. No changes have been made."
+if [[ "$confirmation" != "yes"  ]] && [[ "$confirmation" != "y"  ]]; then
+    printf "${RED}DNS{$NC} change aborted. No changes have been made.\n"
     exit 0
 fi
 
-resolved_conf="/etc/systemd/resolved.conf"
-
 # Uncomment and modify DNS and DNSOverTLS settings
-sudo sed -i -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNS=\).*/\1$dns_provider/" \
+sed -i -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNS=\).*/\1$dns_provider/" \
             -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNSOverTLS=\).*/\1yes/" \
             -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNSSEC=\).*/\1yes/" \
             -e "/^\[Resolve\]/,/^\s*$/ s/^#*\(DNSSECStub=\).*/\1yes/" $resolved_conf
 
 # Add additional DNS lines for NextDNS
 if [ "$choice" -eq 5 ]; then
-    echo "Adding NextDNS configuration to resolved.conf..."
-    sudo tee -a $resolved_conf > /dev/null <<EOL
+    printf "Adding NextDNS configuration to resolved.conf...\n"
+    tee -a $resolved_conf > /dev/null <<EOL
 DNS=$ipv6_provider
 DNS=$dns_provider2
 DNS=$ipv6_provider2
@@ -77,8 +85,8 @@ EOL
 fi
 
 # Enable and start the systemd-resolved service
-if sudo systemctl enable systemd-resolved && sudo systemctl restart systemd-resolved; then
-    echo "You're all set! Your DNS has been set to $provider_name, and DNS-over-TLS has been enabled for security measures."
+if systemctl enable systemd-resolved && systemctl restart systemd-resolved; then
+    printf "You're all set! Your ${RED}DNS${NC} has been set to $provider_name, and ${RED}DNS-over-TLS${NC} has been enabled for security measures."
 else
-    echo "Your DNS provider has not been set due to errors. Do you have systemd-resolved installed on your system?"
+    printf "Your ${RED}DNS${NC} provider has not been set due to errors. Do you have ${RED}systemd-resolved${NC} installed on your system?"
 fi
